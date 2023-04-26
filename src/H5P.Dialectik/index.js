9ee6cffd98582f6a46b5b0728937e41efb61f0bb
Benoit Rognier <benoit.rognier@edukera.com>
2023-04-26 17:36:10 +0200

    fix

---
+++ src/H5P.Dialectik/index.js
@@ -0,0 +1,42 @@
+var H5P = H5P || {};
+
+H5P.Dialectik = (function ($) {
+  /**
+   * Constructor function.
+   */
+  function C(options, id) {
+    this.$ = $(this);
+    // Extend defaults with provided options
+    //this.options = $.extend(true, {}, {
+    //  script: '',
+    //}, options);
+    this.options = options;
+    // Keep provided id.
+    this.id = id;
+  };
+
+  /**
+   * Attach function called by H5P framework to insert H5P content into
+   * page
+   *
+   * @param {jQuery} $container
+   */
+  C.prototype.attach = function ($container) {
+    var self = this;
+    $container.addClass('dialectik-container');
+     // Add root div needed by react.
+    $container.append('<div id="root"></div>')
+    // Add content's react script.
+    const script = document.createElement('script');
+    script.defer = true;
+    script.text = window.atob(this.options.script, 'base64');
+    $container.append(script);
+    // TODO - need to wait for image beeing loaded
+    // For now using timer. Should wait for image is loaded...
+    setTimeout(function () {
+      self.$.trigger('resize');
+    }, 1000);
+  };
+
+  return C;
+})(H5P.jQuery);
